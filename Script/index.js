4460231942c7d7f42b522696339a7d7016df4f11
HosseinNaseran <hossein.naseran@gmail.com>
2026-02-23 23:44:09 +0330

    contact us added

--- Script/index.js
+++ Script/index.js
@@ -4,15 +4,22 @@ const NumberInput = document.getElementById("NumberInput");
 const EmailInput = document.getElementById("EmailInput");
 const MessageInput = document.getElementById("MessageInput");
 
-ContactUsForm.addEventListener("submit", (e) => {  
 
+const PUBLIC_KEY = "JcmtVYfGa1XR-NtqC";
+const SERVICE_ID = "service_vq67zdg";
+const TEMPLATE_ID = "template_o68hm4g";
+
+emailjs.init({
+    publicKey: PUBLIC_KEY,
+});
+
+ContactUsForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const name = NameInput.value.trim();
     const number = NumberInput.value.trim();
     const email = EmailInput.value.trim();
     const message = MessageInput.value.trim();
-
 
     const nameRegex = /^[ا-ی\s , a-zA-Z\s]+$/;
     const numberRegex = /^\d{11}$/;
@@ -32,14 +39,25 @@ ContactUsForm.addEventListener("submit", (e) => {
         alert("لطفاً یک ایمیل معتبر وارد کنید.");
         return;
     }
+
     if (message.length === 0) {
         alert("لطفاً یک پیام وارد کنید.");
         return;
     }
-    alert("پیام شما با موفقیت ارسال شد!");
-
-  
-
-} )
 
 
+    emailjs.send(SERVICE_ID, TEMPLATE_ID, {
+        name: name,
+        number: number,
+        email: email,
+        message: message
+    })
+    .then(() => {
+        alert("پیام شما با موفقیت ارسال شد!");
+        ContactUsForm.reset();
+    })
+    .catch((error) => {
+        console.error(error);
+        alert("خطا در ارسال پیام. لطفاً دوباره تلاش کنید.");
+    });
+});
